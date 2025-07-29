0a9010cc4b87fe811ba77af60bd333463c830b38
Man110700 <phamngocman00@gmail.com>
2025-07-30 01:04:58 +0700

    2nd commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const pool = require("./db");
 const PORT = 5000;
 
 app.use(cors());
 app.use(express.json());
 
 // Sample API route
-app.get('/api/hello', (req, res) => {
+app.get("/api/hello", (req, res) => {
   res.json({ message: "Xin chào từ server NodeJS!" });
 });
 
