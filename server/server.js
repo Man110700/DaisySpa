bec54dbbfbcae4a1a2ccda51d0b1608af54d2b51
Man110700 <phamngocman00@gmail.com>
2025-07-29 22:23:40 +0700

    1st commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Sample API route
+app.get('/api/hello', (req, res) => {
+  res.json({ message: "Xin chào từ server NodeJS!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy ở http://localhost:${PORT}`);
+});
